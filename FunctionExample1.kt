7c29f99a9056d58fe849ca235e4cff9ecf68dc38
Aditya Gupta <113509012+aditya-gupta-GL@users.noreply.github.com>
2023-04-18 17:15:47 +0530

    Example 1 for Function

---
+++ FunctionExample1.kt
@@ -0,0 +1,7 @@
+fun main() {
+    infix fun Int.times(str: String) = str.repeat(this)        
+  println(2 times "Aditya ")                                    
+
+  val pair = "Hello" to "Aman"                          
+  println(pair)
+}
