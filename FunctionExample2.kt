923a2826cb4945a7d68f6a52286c47358213d06e
Aditya Gupta <113509012+aditya-gupta-GL@users.noreply.github.com>
2023-04-18 17:21:04 +0530

    Example 2

---
+++ FunctionExample2.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val numberOfBlueFish = 5
+val numberOfRedFish = 12
+
+println("I have $numberOfBlueFish Blue Fish and $numberOfRedFish Red Fish") 
+println("I have ${numberOfBlueFish + numberOfRedFish} fishs")
+
+}
